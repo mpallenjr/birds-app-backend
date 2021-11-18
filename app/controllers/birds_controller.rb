48da984fbde5820039c6b2de7e9434413ffd30ef
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-11-18 10:59:24 -0600

    create action working on backend. dynamic with params.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,14 @@ class BirdsController < ApplicationController
     index_birds = Bird.all 
     render json: index_birds.as_json 
   end
+  def create
+    create_birds = Bird.create(
+      breed: params[:breed],
+      location: params[:loction],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    render json: create_birds.as_json 
+    
+  end
 end
